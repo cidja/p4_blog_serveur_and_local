bdc70772abd3e87f447506158b055fddb3ae556e
cidja <chgeorgesc@gmail.com>
2019-12-10 17:10:28 +0100

    Create home.php

---
+++ view/frontend/home.php
@@ -0,0 +1,11 @@
+<?php $title = "Accueil"; 
+
+ob_start(); //Commencement de la capture ?> 
+
+<div id="bloc_page">
+    <div id="content_home">
+        <div class="col-12 d-flex justify-content-center">
+        
+        </div>
+    </div>
+</div>
