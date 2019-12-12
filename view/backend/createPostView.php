425673dafa36980d5daf8bd37f7e619b582825bb
cidja <chgeorgesc@gmail.com>
2019-12-12 17:47:15 +0100

    Update createPostView.php
    design form ok

--- view/backend/createPostView.php
+++ view/backend/createPostView.php
@@ -7,30 +7,27 @@ session_start();
     <a class="btn btn-info col mb-4 text-uppercase" href="/p4/index.php?action=backend">Retour à la liste des billets </a>
     <section id="createPost">
         <div class="container-fluid">
-            <div class="row">
             <h1 class="text-center">Création de billet</h1>
-                <form action="/p4/index.php?action=createPostViewConfirm" method="post">
-                    <div class="row">
-                        <label for="title"> Titre du billet :
-                            <input type="text" id="title" name="title" placeholder="Titre" required autofocus />
-                        </label>
+            <form action="/p4/index.php?action=createPostViewConfirm" method="post">
+                <div class="form-row">
+                    <div class="form-group col-12">
+                        <label for="title"> Titre du billet : </label>
+                        <input class="form-control" type="text" id="title" name="title" placeholder="Titre" required autofocus />
                     </div>
-                    <div class="row">
-                        <label for="content">Contenu du billet :
-        <!--en desso    us utilisation des balises pour Tiny MCE pour écrire les posts !-->
-                            <textarea id="content" name="content" placeholder="rentrez votre texte ici" required>Rentrez votre texte du billet </textarea>
-                        </label>
-                    </div>
-                    <input type="submit" value="valider" />
-                </form> 
-            </div>
+                </div>
+                <div class="form-group">
+                    <label for="content">Contenu du billet :</label>
+                    <!--en dessous utilisation des balises pour Tiny MCE pour écrire les posts !-->
+                        <textarea class="form-control" id="content" name="content" placeholder="rentrez votre texte ici" required>Rentrez votre texte du billet </textarea>     
+                </div>
+                <div class="form-group">
+                    <input class="btn btn-success" type="submit" value="valider" />
+                </div>
+            </form> 
         </div>
-</section>
+    </section>
+</div>
 
-    
-
-    <!--lien pour retourner à la liste des billets !-->
-    </div>
     <?php  
     $content = ob_get_clean();
 
