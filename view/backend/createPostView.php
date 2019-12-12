c4305770d9b5911d635a80e314899a975068c956
cidja <chgeorgesc@gmail.com>
2019-12-12 16:14:41 +0100

    Update createPostView.php

--- view/backend/createPostView.php
+++ view/backend/createPostView.php
@@ -3,20 +3,34 @@ session_start();
  $title = "Création de post"; 
 
  ob_start(); ?> <!--commencement de la capture pour créer la variable $content qu'on insère dans templateBackend.php !-->
+<div class="container-fluid">
+    <a class="btn btn-info col mb-4 text-uppercase" href="/p4/index.php?action=backend">Retour à la liste des billets </a>
+    <section id="createPost">
+        <div class="container-fluid">
+            <div class="row">
+            <h1 class="text-center">Création de billet</h1>
+                <form action="/p4/index.php?action=createPostViewConfirm" method="post">
+                    <div class="row">
+                        <label for="title"> Titre du billet :
+                            <input type="text" id="title" name="title" placeholder="Titre" required autofocus />
+                        </label>
+                    </div>
+                    <div class="row">
+                        <label for="content">Contenu du billet :
+        <!--en desso    us utilisation des balises pour Tiny MCE pour écrire les posts !-->
+                            <textarea id="content" name="content" placeholder="rentrez votre texte ici" required>Rentrez votre texte du billet </textarea>
+                        </label>
+                    </div>
+                    <input type="submit" value="valider" />
+                </form> 
+            </div>
+        </div>
+</section>
 
- <h1>Création de billet</h1>
-
-    <form action="/p4/index.php?action=createPostViewConfirm" method="post">
-        <label for="title"> Titre du billet : </label>
-            <input type="text" id="title" name="title" placeholder="Titre" required autofocus />
-        <label for="content">Contenu du billet : </label>    
- <!--en dessous utilisation des balises pour Tiny MCE pour écrire les posts !-->
-        <textarea id="content" name="content" placeholder="rentrez votre texte ici" required>Rentrez votre texte du billet </textarea>
-        <input type="submit" value="valider" />
-    </form> 
+    
 
     <!--lien pour retourner à la liste des billets !-->
-    <a href="/p4/index.php?action=backend">Retour à la liste des billets </a>
+    </div>
     <?php  
     $content = ob_get_clean();
 
