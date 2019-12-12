ca74c1500fedfbb3f72a709789409432c1381c58
cidja <chgeorgesc@gmail.com>
2019-12-12 10:16:58 +0100

    Update updateViewBackend.php

--- view/backend/updateViewBackend.php
+++ view/backend/updateViewBackend.php
@@ -1,22 +1,39 @@
 <?php 
 
 if(isset($_SESSION["user"]) && isset($_SESSION["mdp"])){ //on vérifie que l'on a bien les id et mdp pour acceder à l'interface d'admin
-    $title = "Admin mon blog"; 
+    $title = "Modification d'un billet"; 
 
     ob_start(); ?>
-
-    <h3>Modification du post </h3>
-    <form action="/p4/index.php?action=updatePostConfirm&amp;id=<?= $_GET["id"]; ?>" method="post">
-        <label for="title"> Titre du billet : </label>
-            <input type="text" id="title" name="title" value="<?= htmlspecialchars($post['title']);?>" required autofocus />
-        <label for="content">Contenu du billet : </label>    
- <!--en dessous utilisation des balises pour Tiny MCE pour écrire les posts !-->
-        <textarea id="content" name="content" placeholder="rentrez votre texte ici" required><?= nl2br(htmlspecialchars_decode($post["content"])); ?> </textarea>
-        <input type="submit" value="valider" />
-    </form> 
-
-    <!--lien pour retourner à la liste des billets !-->
-    <a href="/p4/index.php?action=backend">Retour à la liste des billets </a>
+    <div class="container">
+        <!--lien pour retourner à la liste des billets !-->
+        <a class="btn btn-info col mb-4 text-uppercase" href="/p4/index.php?action=backend">Retour à la liste des billets </a>
+        <section id="updatePost">
+            <div class="container bg-success">
+                <div class="row justify-content-center text-uppercase text-white">
+                    <div class="mb-2 mt-2">
+                        <h3 class="text-center">Modification du post </h3>
+                        <form class="form-login" action="/p4/index.php?action=updatePostConfirm&amp;id=<?= $_GET["id"]; ?>" method="post">
+                            <div class="text-center">
+                                <label for="title"> Titre du billet :
+                                    <input type="text" id="title" name="title" value="<?= $post['title'];?>" required autofocus /> 
+                                </label>
+                            </div>
+                            <div class="text-center">
+                                <label for="content">Contenu du billet : 
+                                    <!--en dessous utilisation des balises pour Tiny MCE pour écrire les posts !-->
+                                    <textarea id="content" name="content" placeholder="rentrez votre texte ici" required><?= nl2br(htmlspecialchars_decode($post["content"])); ?> </textarea>
+                                </label>
+                            </div>
+                            <div class="text-center">
+                                <input class="btn btn-secondary" type="submit" value="valider" />
+                            </div>
+                        </form> 
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
+    
     <?php  
     $content = ob_get_clean();
 
